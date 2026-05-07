cfd9ce23e4f79a82154f94e986f7bf85c1deef82
Ojas Kushwah <197230141+OjasSingh1o1@users.noreply.github.com>
2026-05-07 13:15:09 +0000

    variables commit

---
+++ basics_1o1/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1044
+let accountEmail = "thealchemist@curator.com"
+var accountPassword = "ViniViciVidi" // shi too outdated we dont use it nomo to define variables, use let instead
+accountLocation = "Malibu"
+let accountName
+
+// accountId = 9 // BOMBACLAAAAAQAT (shi aint possible gng)
+
+accountEmail = "culiusjaesar@printout.com"
+accountPassword = "luluLEMON"
+accountLocation = "Miami"
+accountName
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountLocation, accountName])
