bac552d94f5277dceba7d7c7f81adf9621c8d7fd
Ojas Kushwah <197230141+OjasSingh1o1@users.noreply.github.com>
2026-05-07 13:33:05 +0000

    data type

---
+++ basics_1o1/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// alert("Waddup Biyatch")
+console.log("Waaddup Biyatch");
+console.log("6" + "7")
+
+let gamingconsole = "ps4"
+let currStutus = "shit is over now"
+let year = 2025
+let isValid = false
+let geekstatus = null
+
+console.table([gamingconsole, currStutus])
+console.log(typeof gamingconsole);
